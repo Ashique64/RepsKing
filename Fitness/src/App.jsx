d743f16a07574063dcc8872e065378e3c39b557b
Ashique <ashique9496@gmail.com>
2025-03-11 17:39:47 +0530

    about section mobile view completed

--- Fitness/src/App.jsx
+++ Fitness/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import About from "./Pages/About/About";
 import Hero from "./Pages/Hero/Hero";
+import Services from "./Pages/Services/Services";
 
 function App() {
     return (
         <>
             <Hero />
             <About />
-            
+            <Services/>
         </>
     );
 }
